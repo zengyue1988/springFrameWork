5cbf40fd7082226cceba2b1a47cfa4b79c9da564
Moon <404955453@qq.com>
2018-11-08 22:47:35 +0800

    Signed-off-by: Moon <404955453@qq.com>

--- spring-context-sample/src/main/java/com/moon/springBoot/Application.java
+++ spring-context-sample/src/main/java/com/moon/springBoot/Application.java
@@ -1,10 +1,15 @@
 package com.moon.springBoot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			
+		/////
+		SpringApplication.run(Application.class, args);
+		///
 	}
-
+	
 }
