e147017101e553a9c7833c82a0ba2f8e044ebc31
'Moon' <404955453@qq.com>
2018-11-22 21:37:34 +0800

    spring transaction
    Change-Id: I6280dd51f4710e064b95b8e8132ccb4a6065c8ed

---
+++ spring-context-sample/src/main/java/com/moon/transactions/springTransaction/service/BookingService.java
@@ -0,0 +1,40 @@
+package com.moon.transactions.springTransaction.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookingService {
+	
+	private final static Logger logger = LoggerFactory.getLogger(BookingService.class);
+
+    private final JdbcTemplate jdbcTemplate;
+    
+    public BookingService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    
+    public void book(String... persons) {
+    	for (String person : persons) {
+    		logger.info("Booking " + person + " in a seat...");
+    		jdbcTemplate.update("insert into BOOKINGS(FIRST_NAME) values (?)", person);
+    	}
+    }
+    
+    public List<String> findAllBookings(){
+    	List<String> nameList = new ArrayList<String>();
+    	//List<Map<String, Object>> rows = jdbcTemplate.queryForList("select FIRST_NAME from BOOKINGS");
+    	List<Map<String, Object>> rows = jdbcTemplate.queryForList("select * from BOOKINGS");
+    	for (Map<String, Object> row : rows) {
+    		nameList.add((String) row.get("FIRST_NAME"));
+    	}
+    	return nameList;
+    }
+
+}
