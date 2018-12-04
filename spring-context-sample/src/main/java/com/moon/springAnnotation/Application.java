a7290b7d0bd99b4c9d39376a4f7cd961dd0f689a
'Moon' <bet_sweet_zy@outlook.com>
2018-12-04 19:58:15 +0800

    get bean from annotation
    Change-Id: If30b18d70c16232d61ff0d95731e6dcc1e310833

--- spring-context-sample/src/main/java/com/moon/springAnnotation/Application.java
+++ spring-context-sample/src/main/java/com/moon/springAnnotation/Application.java
@@ -1,16 +1,21 @@
 package com.moon.springAnnotation;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
 import com.moon.springAnnotation.service.HelloService;
 
+@ComponentScan(value="com.moon.springAnnotation")
 public class Application {
 
 	/**
-	 * @param args
+	 * <context:component-scan base-package="cn.outofmemory.spring" use-default-filters="false">
+	 * 	<context:include-filter type="regex" expression="cn\.outofmemory\.spring\.[^.]+(Dao|Service)"/>
+	 * </context:component-scan>
 	 */
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("/com/moon/springAnnotation/resources/*.xml");
+		//ApplicationContext context = new ClassPathXmlApplicationContext("/com/moon/springAnnotation/resources/*.xml");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 		HelloService helloService = context.getBean("helloService", HelloService.class);
 		System.out.println("*****from host");
 		System.out.println(helloService.sayHello());
