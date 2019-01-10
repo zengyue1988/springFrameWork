4f0d971152d65b4ee2f433aa4df695a44f58a161
'Moon' <bet_sweet_zy@outlook.com>
2019-01-10 21:23:05 +0800

    add security support and springboot get param
    Change-Id: I247a1665a55e1ebc7614f525fa45baa89c7ce2c1

--- spring-context-sample/src/main/java/com/moon/accessDate/mySQL/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.moon.accessDate.mySQL.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.moon.accessDate.mySQL.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
--- spring-context-sample/src/main/java/com/moon/springBoot/controller/GreetingController.java
+++ spring-context-sample/src/main/java/com/moon/springBoot/controller/GreetingController.java
@@ -2,16 +2,20 @@ package com.moon.springBoot.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.moon.accessDate.JPA.entity.Customer;
 import com.moon.springBoot.bean.Greeting;
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
+    private static final String template2 = "Hello, %s %s! %s";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
@@ -19,4 +23,15 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+    
+	@RequestMapping("/customer/new/{customerNo}")
+	public @ResponseBody Greeting createCustomer(
+			@PathVariable("customerNo") Integer customerNo,
+			@RequestParam("firstName") String firstName,
+			@RequestParam(value="lastName",required=false,defaultValue="Moon") String lastName
+			) {
+		return new Greeting(counter.incrementAndGet(),
+                String.format(template2, firstName, lastName, customerNo));
+	}
+
 }
