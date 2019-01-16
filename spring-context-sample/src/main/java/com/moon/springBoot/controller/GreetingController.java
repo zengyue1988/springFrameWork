a405aa60a0511ec2f4d78a03528dd32b4ac0ee7e
'Moon' <bet_sweet_zy@outlook.com>
2019-01-16 22:55:36 +0800

    add swagger support
    Change-Id: I5acb024a0f5f7f92d2ab82ba3023c8a730bb00b0

--- spring-context-sample/src/main/java/com/moon/springBoot/controller/GreetingController.java
+++ spring-context-sample/src/main/java/com/moon/springBoot/controller/GreetingController.java
@@ -8,9 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.moon.accessDate.JPA.entity.Customer;
 import com.moon.springBoot.bean.Greeting;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.SwaggerDefinition;
+
+@SwaggerDefinition(
+	schemes = {SwaggerDefinition.Scheme.HTTPS}
+)
+@Api(
+		value = "API information",
+		consumes = "application/json",
+		produces = "application/json"
+)
 @RestController
 public class GreetingController {
 
@@ -23,7 +36,13 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
-    
+
+    @ApiOperation(value = "create a customer")
+    @ApiResponses(value= {
+    		@ApiResponse(code=400, message="Invalid request"),
+    		@ApiResponse(code=404, message="api not found"),
+    		@ApiResponse(code=500, message="Server error"),
+    })
 	@RequestMapping("/customer/new/{customerNo}")
 	public @ResponseBody Greeting createCustomer(
 			@PathVariable("customerNo") Integer customerNo,
